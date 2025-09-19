879ad587e883729938cf3c275d54dfe893d44d2a
Emdaduljs <emdaduljs@gmail.com>
2025-09-20 02:26:26 +0600

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# utils package initializer
-# expose the helper modules for simple imports
-
-from . import mockup
-from . import render
-from . import mapping
-from . import export
-
-__all__ = ["mockup", "render", "mapping", "export"]
